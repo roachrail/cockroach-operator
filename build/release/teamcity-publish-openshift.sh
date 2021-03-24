76eeb117ceffb1558468e7edd01431bf079bba4a
Rail Aliiev <rail@iqchoice.com>
2021-03-24 03:05:56 +0000

    Revert "fixes"
    This reverts commit eceb38a4acbbd068b578cc78bffdcac1621424ab.

--- build/release/teamcity-publish-openshift.sh
+++ build/release/teamcity-publish-openshift.sh
@@ -19,8 +19,7 @@ source "$(dirname "${0}")/teamcity-support.sh"
 
 
 tc_start_block "Variable Setup"
-version_file="$(dirname "${0}")/../../version.txt"
-VERSION=$(cat $version_file)
+VERSION=$(cat version.txt)
 # Matching the version name regex from within the cockroach code except
 # for the `metadata` part at the end because Docker tags don't support
 # `+` in the tag name.
